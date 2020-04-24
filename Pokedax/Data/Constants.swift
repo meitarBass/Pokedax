eadb4b318e028a332ef5826c63daa33a5a6c92db
Meitar Basson <meitarbass@gmail.com>
2020-04-25 00:54:09 +0300

    First commit

---
+++ Pokedax/Data/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Pokedax
+//
+//  Created by Meitar Basson on 25/04/2020.
+//  Copyright © 2020 Meitar Basson. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "https://pokeapi.co/api/v2/"
